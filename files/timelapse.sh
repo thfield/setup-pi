353da017fa40e2ce27e15931283c77e87cd08305
Tyler Field <thfield@gmail.com>
2019-08-08 22:57:30 -0700

    fix typo

--- files/timelapse.sh
+++ files/timelapse.sh
@@ -2,7 +2,7 @@
 DATE=$(date +"%Y-%m-%d_%H%M%S")
 
 # if camera is right-side up
-raspistill -w 1920 -h 1080 -ex auto -awb auto -n -t 50000 -tl 10000 -o /home/pi/timelapse/$DATE.jpg
+raspistill -w 1920 -h 1080 -ex auto -awb auto -n -o /home/pi/timelapse/${DATE}img%04d.jpg
 
 # if camera is upside down
 #raspistill -w 1920 -h 1080 -ex auto -awb auto -vf -hf -n -o /home/pi/Pictures/timelapse/$DATE.jpg
--- setup-temperature.sh
+++ setup-temperature.sh
@@ -5,9 +5,13 @@ mkdir -p /home/pi/temperatures
 mv files/temperatures.sh /home/pi/bin/temperature.sh
 chmod +x /home/pi/bin/temperature.sh
 
-sudo mv files/ftemp.service /lib/systemd/system/tftemp.service
+sudo mv files/tftemp.service /lib/systemd/system/tftemp.service
 sudo chmod 644 /lib/systemd/system/tftemp.service
 
 sudo systemctl daemon-reload
 sudo systemctl enable tftemp.service
 sudo systemctl start tftemp.service
+
+# https://crontab.guru/
+# add to crontab
+# * * * * * /home/pi/bin/temperature.sh
